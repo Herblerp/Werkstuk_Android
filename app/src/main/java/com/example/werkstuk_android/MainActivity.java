4a76405de4217725b16929b8a44b7983d704601b
Quentin <Quentin.vekeman@gmail.com>
2020-01-03 19:55:48 +0100

    Implemented button on main activity

--- app/src/main/java/com/example/werkstuk_android/MainActivity.java
+++ app/src/main/java/com/example/werkstuk_android/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MainActivity","App sais hello!");
+    }
+
+    public void button_click(View view) {
+        Button button = findViewById(R.id.btn_homepage_lists);
+        button.setText("You clicked me!");
     }
 }
