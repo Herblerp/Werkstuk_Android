3e2077d754b82c4fd7ecfbfaa0fed9ed3fc2c875
Quentin <Quentin.vekeman@gmail.com>
2020-01-05 20:32:40 +0100

    Implemented random recipe

--- app/src/main/java/com/example/werkstuk_android/AppRepository.java
+++ app/src/main/java/com/example/werkstuk_android/AppRepository.java
@@ -3,41 +3,62 @@ package com.example.werkstuk_android;
 
 import android.app.Application;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 
 import java.util.List;
 import java.util.Observable;
+import java.util.concurrent.ExecutionException;
 
-public class AppRepository {
+public class AppRepository{
 
     private UserListDao mUserListDao;
     private UserListEntryDao mUserListEntryDao;
 
-    public AppRepository (Application application) {
+    public AppRepository(Application application) {
         AppDatabase db = AppDatabase.getDatabase(application);
         mUserListDao = db.userListDao();
         mUserListEntryDao = db.userListEntryDao();
     }
 
-    public void insertList(UserList list){
+    public void insertList(UserList list) {
 
         mUserListDao.insertList(list);
     }
 
-    public List<ListWithEntries> getLists(){
-       return mUserListDao.getAll();
+    public List<ListWithEntries> getLists() {
+        return mUserListDao.getAll();
     }
 
-    public ListWithEntries getListById(int id)
-    {
+    public ListWithEntries getListById(int id) {
         return mUserListDao.getById(id);
     }
 
-    public void insertEntry(UserListEntry entry)
-    {
+    public void insertEntry(UserListEntry entry) {
         mUserListEntryDao.insertEntry(entry);
+    }
+
+    public String getRandomRecipeAsync() {
+
+        //Some url endpoint that you may have
+        String myUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+        //String to place our result in
+        String result = "";
+
+        HttpGetRequest getRequest = new HttpGetRequest();
+
+        try {
+            result = getRequest.execute(myUrl).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
 
--- app/src/main/java/com/example/werkstuk_android/RecipeAvtivity.java
+++ app/src/main/java/com/example/werkstuk_android/RecipeAvtivity.java
@@ -1,14 +1,42 @@
+// Source: https://medium.com/@JasonCromer/android-asynctask-http-request-tutorial-6b429d833e28
 package com.example.werkstuk_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class RecipeAvtivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+public class RecipeAvtivity extends AppCompatActivity{
+
+    private AppRepository repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_avtivity);
+
+        TextView recipeName = findViewById(R.id.recipe_name);
+        TextView recipeDescription = findViewById(R.id.recipe_description);
+
+        repo = new AppRepository(getApplication());
+        String recipeString = repo.getRandomRecipeAsync();
+
+        try {
+            JSONObject jsonResult = new JSONObject(recipeString);
+            JSONArray mealArray = jsonResult.getJSONArray("meals");
+            JSONObject meal = mealArray.getJSONObject(0);
+
+            recipeName.setText((meal.getString("strMeal")));
+            recipeDescription.setText(meal.getString("strInstructions"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
